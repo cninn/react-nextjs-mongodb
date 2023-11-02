03379ff3ab0e7821d0e2b08af93f5bc506c186cd
can inan <cninnworks@gmail.com>
2023-11-02 12:28:20 +0300

    pushing

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
+/*     images:{
         domains:[
             "images.pexels.com",
             "images.unsplash.com"
         ]
-    }
+    } */
 }
 
 module.exports = nextConfig
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -4,39 +4,35 @@ import blog from 'public/code.jpeg'
 import Image from 'next/image'
 import Link from 'next/link'
 
-const Blog = () => {
+
+
+async function getData(){
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+
+  if(!res.ok){
+    throw new Error('veri yüklemesi başarısız')
+  }
+  return res.json();
+}
+
+const Blog = async () => {
+
+  const data = await getData();
   return (
     <div className={styles.container}>
-
-      <Link href={'/blog/testId'} className={styles.containerr}>
+        {data.map(item=>(
+      <Link href={'/blog/testId'} className={styles.containerr} key={item.id}>
       <div className={styles.imgContainer}>
     <Image className={styles.img} src={blog} alt='...'/>
       </div>
       <div className={styles.content}>
-        <h1 className={styles.title}>Lorem ipsum dolor sit amet.</h1>
-        <p className={styles.desc}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum quam facilis laborum dolor sint, laboriosam tempore nostrum quas pariatur debitis nesciunt facere alias quo excepturi, quasi temporibus. Obcaecati, aliquid earum!</p>
+        <h1 className={styles.title}>{item.title}</h1>
+        <p className={styles.desc}>{item.body}</p>
       </div>
       </Link>
+        ))}
 
-      <Link href={'/blog/testId'} className={styles.containerr}>
-      <div className={styles.imgContainer}>
-    <Image className={styles.img} src={blog} alt='...'/>
-      </div>
-      <div className={styles.content}>
-        <h1 className={styles.title}>Lorem ipsum dolor sit amet.</h1>
-        <p className={styles.desc}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum quam facilis laborum dolor sint, laboriosam tempore nostrum quas pariatur debitis nesciunt facere alias quo excepturi, quasi temporibus. Obcaecati, aliquid earum!</p>
-      </div>
-      </Link>
 
-      <Link href={'/blog/testId'} className={styles.containerr}>
-      <div className={styles.imgContainer}>
-    <Image className={styles.img} src={blog} alt='...'/>
-      </div>
-      <div className={styles.content}>
-        <h1 className={styles.title}>Lorem ipsum dolor sit amet.</h1>
-        <p className={styles.desc}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum quam facilis laborum dolor sint, laboriosam tempore nostrum quas pariatur debitis nesciunt facere alias quo excepturi, quasi temporibus. Obcaecati, aliquid earum!</p>
-      </div>
-      </Link>
     </div>
   )
 }
